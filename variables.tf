f2e51fa0b84ebae48c8dba39b31c62b817d547ca
ikauzak <brunokazuaki@gmail.com>
2023-07-01 19:39:35 -0300

    remove var

--- variables.tf
+++ variables.tf
@@ -8,11 +8,6 @@ variable "location" {
   description = "(Required) Specifies the supported Azure location where the resource exists. Changing this forces a new resource to be created."
 }
 
-variable "name_resource_group" {
-  type        = string
-  description = "(Required) The name of the resource group in which to create the storage account. Changing this forces a new resource to be created."
-}
-
 variable "sa_settings" {
   type = object({
     account_tier             = string
